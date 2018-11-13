a4bf7946ff8310a25874407f7f7fa2354d534b30
Wolgan Ens <ens.wolgan@gmail.com>
2018-11-13 16:32:55 -0200

    pasta publica

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'client', 'build')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/researchers', cors(), researchersRouter)
 app.get('/*', cors(), function(req, res, next) {
